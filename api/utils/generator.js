188c454e9c108b5ea91807880985479aafc5b9cc
phimtarwat <phimtarwat@gmail.com>
2025-09-17 10:45:07 +0700

    Update generator.js

--- api/utils/generator.js
+++ api/utils/generator.js
@@ -1,1 +1,12 @@
+export function generateUniqueId(existingIds) {
+  let id;
+  do {
+    id = Math.floor(10000 + Math.random() * 90000).toString();
+  } while (existingIds.includes(id));
+  return id;
+}
+
+export function generateToken() {
+  return Math.floor(10000 + Math.random() * 90000).toString();
+}
 
